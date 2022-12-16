09dadd1d0614f4fd3f11cb35487bbafc9734af8c
Caroline Crandell <cecrandell19@gmail.com>
2022-12-16 15:52:54 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Machine Learning A-Z (Codes and Datasets)
+
+## Caroline Crandell
+
+This repository demonstrates my learnings about machine learning, as I work through a course on [Udemy](https://www.udemy.com/course/machinelearning/).
+
+### Table of Contents 
+
+* Section 1 - Introduction
+* Section 2 - Data Preprocessing
---
+++ section_2.py
@@ -0,0 +1,24 @@
+# Section 2
+
+# The Machine Learning Process
+
+# Data Pre-Processing
+
+# • Import the data
+# • Clean the data
+# • Split into training and test sets
+
+# Modelling (the fun part)
+
+# • Build the model
+# • Train the model
+# • Make predictions
+
+# Evaluation
+
+# • Calculate performance metrics
+# • Make a verdict
+
+# Predicting Car Sales
+
+# Dependent variables: mileage and age
